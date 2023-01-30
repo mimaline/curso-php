cb5c338c32918df78626a3e845a934d3b0f7be61
Yasmim Aline Roeder <yasmim.aline97@hotmail.com>
2023-01-30 16:08:49 -0300

    ok

--- Aula 01-30/arquivo.php
+++ Aula 01-30/arquivo.php
@@ -1,13 +1,38 @@
 <?php
 
-echo 'Inicio do arquivo.php';
-echo '<br>';
-echo 'pegando dados dos parametros da url';
-echo '<br>';
+// echo 'Inicio do arquivo.php';
+// echo '<br>';
+// echo 'pegando dados dos parametros da url';
+// echo '<br>';
 
-$nome_parametro = $_GET["nome"];
-$idade_parametro = $_GET["idade"];
+$nome_parametro = "NOME EM BRANCO";
+$idade_parametro = "IDADE EM BRANCO";
 
-$frase = "Meu nome é " . $nome_parametro . ' e tenho ' . $idade_parametro . ' anos!';
+$validaDados = true;
 
-echo $frase;
+if(isset($_GET["nome"])){
+    $nome_parametro = $_GET["nome"];
+} else {
+    $validaDados = false;
+}
+if(isset($_GET["idade"])){
+    $idade_parametro = $_GET["idade"];
+} else {
+    $validaDados = false;
+}
+
+// se a validação de dados for true:
+
+    if($validaDados) {
+        $frase = "Meu nome é " . $nome_parametro . ' e tenho ' . $idade_parametro . ' anos!';
+
+        echo $frase;
+    } else {
+        echo 'Parametros Inválidos!';
+    }
+
+
+
+
+
+
